eb46c4b1f6dcd57ad921b00cab109a0a327b725a
Adham Ayman <120693599+AdhamAymanElsayed@users.noreply.github.com>
2023-04-08 04:23:28 +0200

    Add files via upload

---
+++ Pages/3_📩_Contacts.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header(':mailbox: Get in touch with me!')
+st.write('_______')
+st.write('* Github Link : https://github.com/AdhamAymanElsayed')
+st.write('* linkedin Link : https://www.linkedin.com/in/adhamfarid/')
